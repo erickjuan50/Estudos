70bb1dc43cf0590318bc26858ebf63d4a4137d71
Erick Juan <68560717+erickjuan50@users.noreply.github.com>
2024-11-27 14:42:01 -0300

    ATT

--- Atividades/RocketSeat/JavaScript/dom/script.js
+++ Atividades/RocketSeat/JavaScript/dom/script.js
@@ -48,3 +48,77 @@ console.log(guest);
 console.log(guest.textContent); //Retorna o conteúdo visível e oculto
 console.log(guest.innerText); //Retorna o conteúdo visível
 console.log(guest.innerHTML); //Retorna o HTML como texto
+
+
+// Criando elementos
+const newGuest = document.createElement("li")
+
+const guestName = document.createElement("span")
+
+guestName.textContent = "Teste123456"
+
+newGuest.append(guestName)
+
+console.log(newGuest)
+
+// Manipulação de atributos
+const input1 = document.querySelector("#input")
+input1.setAttribute("placeholder", "teste1234")
+// input.setAttribute("disabled", true)
+input1.removeAttribute("name")
+
+// Event listener
+window.addEventListener("load", () => {
+    console.log("Página carregada")
+})
+
+let contador = 0;
+addEventListener("click", (event) => {
+    contador++;
+    // console.log(contador)
+})
+
+addEventListener("click", (i) => {
+    // i.preventDefault()
+    // console.log(i.target)
+})
+
+const button = document.querySelector("button")
+button.addEventListener("click", (event) => {
+    console.log("clicou")
+})
+
+// Lidando com um evento especifico
+
+const form = document.querySelector("form")
+form.onsubmit = (event) => {
+    event.preventDefault()
+    console.log("Submit")
+}
+
+// event Listener em inputs
+
+const input2 = document.querySelector("#input")
+input2.addEventListener("keydown", (event) => {
+    //keydown - captura tudo, ctrl, shift, TUDO, quando for pressionado.
+    // console.log(event, "keydown")
+})
+
+input2.addEventListener("keypress", (event) => {
+    //keypress - captura quando uma tecla do tipo caractere for pressionada (letras, números, pontos...)
+    // console.log(event, "keypress")
+})
+
+// Expressões regulares
+input2.addEventListener("input", (event) => {
+    const regex = /\D+/g
+    const value = input2.value
+    console.log(value.match(regex))
+
+    const isValid = regex.test(value)
+    console.log(isValid)
+
+    
+})
+
+
--- LAWD/Nucleo/Re Vigenere/vigenere.js
+++ LAWD/Nucleo/Re Vigenere/vigenere.js
@@ -161,3 +161,4 @@ document.getElementById("decryptButton").addEventListener("click", function() {
 
 
 
+CryptVigenere.encryptWord()
